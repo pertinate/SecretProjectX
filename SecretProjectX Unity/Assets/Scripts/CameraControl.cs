8e740383a89a7e350ffc22095c802ef06c63eb12
pertinate <ppertinate@gmail.com>
2015-12-08 12:38:56 -0600

    no message

---
+++ SecretProjectX Unity/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+    public int currentView
+    {
+        get
+        {
+            return currentView;
+        }
+        set
+        {
+            if (value > 0 || value <= 4)
+                currentView = value;
+        }
+    }
+    public void SetCurrentView()
+    {
+
+    }
+}
