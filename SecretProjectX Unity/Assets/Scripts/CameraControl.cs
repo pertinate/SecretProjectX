889fb3005705dd927ef867eceb720a18d7cfae66
pertinate <ppertinate@gmail.com>
2015-12-13 16:40:51 -0600

    Use Direction.(Directional Swipe).GetInput() if you need to detect a swipe. Working on CameraControl feel free to contribute to it.

--- SecretProjectX Unity/Assets/Scripts/CameraControl.cs
+++ SecretProjectX Unity/Assets/Scripts/CameraControl.cs
@@ -2,20 +2,46 @@
 using System.Collections;
 
 public class CameraControl : MonoBehaviour {
-    public int currentView
+    public Quaternion[] rotations = new Quaternion[] { new Quaternion(0, 0, 0, 0), new Quaternion(0, 90, 0, 0), new Quaternion(0, 180, 0, 0), new Quaternion(0, 270, 0 ,0), new Quaternion(0, 360, 0, 0)};
+
+    public int currentRotation;
+
+    private void Start()
     {
-        get
+        Debug.Log(Direction.Left);
+        for(int i = 0; i < rotations.Length; i++)
         {
-            return currentView;
-        }
-        set
-        {
-            if (value > 0 || value <= 4)
-                currentView = value;
+            if (this.transform.rotation == rotations[i])
+                currentRotation = i;
         }
     }
-    public void SetCurrentView()
+    private void Update()
     {
-
+        Debug.Log(currentRotation);
+        ControlCamera();
+        if (this.transform.rotation == rotations[currentRotation])
+            StopAllCoroutines();
+    }
+    private void ControlCamera()
+    {
+        if (Direction.Left.GetInput())
+        {
+            currentRotation++;
+            transform.rotation = Quaternion.Lerp(this.transform.rotation, rotations[currentRotation], Time.deltaTime * 0.06f);
+        }
+        else if(Direction.Right.GetInput() || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Debug.Log("pressed");
+            currentRotation--;
+            StartCoroutine(rotateTo());
+        }
+    }
+    private IEnumerator rotateTo()
+    {
+        while (true)
+        {
+            transform.rotation = Quaternion.LerpUnclamped(transform.rotation, rotations[currentRotation], Time.deltaTime * 0.06f);
+            yield return null;
+        }
     }
 }
---
+++ SecretProjectX Unity/Assets/Scripts/SwipeInputs.cs
@@ -0,0 +1,154 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Direction
+{
+    Left,
+    Right,
+    Up,
+    Down,
+    DRight,//Diagonal Right Down
+    DLeft,//Diagonal Left Down
+    URight,//Diagonal Right Up
+    Uleft//Diagonal Left Up
+}
+public static class DirectionExtension
+{
+    public static bool GetInput(this Direction en)
+    {
+        return SwipeInputs.GetInput(en);
+    }
+}
+public class SwipeInputs : MonoBehaviour {
+    private static Vector2 startPos;
+    private static bool isSwiping = false;
+    private static float minDist = 30f;
+    public float minDistSwipe
+    {
+        get { return minDist; }
+        set { minDist = value; }
+    }
+
+
+    public static bool GetInput(Direction en)
+    {
+        if (Input.touchCount > 0)
+        {
+            foreach(Touch touch in Input.touches)
+            {
+                switch (touch.phase)
+                {
+                    case TouchPhase.Began:
+                        isSwiping = true;
+                        startPos = touch.position;
+                        break;
+                    case TouchPhase.Ended:
+                        float gestureDist = (touch.position - startPos).magnitude;
+                        if(isSwiping && gestureDist > minDist)
+                        {
+                            Vector2 direction = touch.position - startPos;
+                            switch (en)
+                            {
+                                case Direction.Left: case Direction.Right:
+                                    if (Mathf.Abs(direction.normalized.x) > 0.65f)
+                                    {
+                                        if (en == Direction.Left)
+                                        {
+                                            if (Mathf.Sign(direction.x) > 0)
+                                                return true;
+                                            else
+                                                return false;
+                                        }
+                                        else if (en == Direction.Right)
+                                        {
+                                            if (Mathf.Sign(direction.x) > 0)
+                                                return false;
+                                            else
+                                                return true;
+                                        }
+                                        else
+                                        {
+                                            return false;
+                                        }
+                                    }
+                                    break;
+                                case Direction.Up: case Direction.Down:
+                                    if (Mathf.Abs(direction.normalized.y) > 0.65f)
+                                    {
+                                        if (en == Direction.Up)
+                                        {
+                                            if (Mathf.Sign(direction.y) > 0)
+                                                return true;
+                                            else
+                                                return false;
+                                        }
+                                        else if (en == Direction.Down)
+                                        {
+                                            if (Mathf.Sign(direction.y) > 0)
+                                                return false;
+                                            else
+                                                return true;
+                                        }
+                                        else
+                                        {
+                                            return false;
+                                        }
+                                    }
+                                    break;
+                                case Direction.DRight: case Direction.URight:
+                                    if(Mathf.Sign(direction.x) > 0)
+                                    {
+                                        if (en == Direction.URight)
+                                        {
+                                            if (Mathf.Sign(direction.y) > 0)
+                                                return true;
+                                            else
+                                                return false;
+                                        }
+                                        else if (en == Direction.DRight)
+                                        {
+                                            if (Mathf.Sign(direction.y) > 0)
+                                                return false;
+                                            else
+                                                return true;
+                                        }
+                                        else
+                                            return false;
+                                    }
+                                    break;
+                                case Direction.DLeft: case Direction.Uleft:
+                                    if(!(Mathf.Sign(direction.x) > 0))
+                                    {
+                                        if (en == Direction.Uleft)
+                                        {
+                                            if (Mathf.Sign(direction.y) > 0)
+                                                return true;
+                                            else
+                                                return false;
+                                        }
+                                        else if (en == Direction.DLeft)
+                                        {
+                                            if (Mathf.Sign(direction.y) > 0)
+                                                return false;
+                                            else
+                                                return true;
+                                        }
+                                        else
+                                            return false;
+                                    }
+                                    break;
+                                default:
+                                    return false;
+                            }
+                        }
+                        break;
+                    case TouchPhase.Canceled:
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        return false;
+    }
+}
